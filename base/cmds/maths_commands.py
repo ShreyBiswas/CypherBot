24962d46c10895310a36e897040d21eac8cc88a3
StreakSharn <74095821+StreakSharn@users.noreply.github.com>
2021-01-22 20:17:19 +0000

    generic function

--- base/cmds/maths_commands.py
+++ base/cmds/maths_commands.py
@@ -5,13 +5,14 @@ import math
 
 @bot.command(name='loglist', help='Logs an entire list of numbers at once!\nFormat: .loglist 1 2 3 4')
 async def on_message(context, *args: int):
-    answer = [log(n) for n in args]
-    await context.send(str(answer))
+    response = [log(n) for n in args]
+    await context.send(str(response))
 
 
-
+#* GENERIC FUNCTION
 @bot.command(name='', help='')
-async def on_message(context, *args):
-    #
-    await context.send(str(reply))
+async def on_message(context, *args): #replace args, add kwargs if needed
+    #do something to args or kwargs
+
+    await context.send(response)
 
