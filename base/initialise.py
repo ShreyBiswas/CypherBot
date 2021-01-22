269c7dee66a0e8ee45be3ab7c6fa29480b91fdcd
StreakSharn <74095821+StreakSharn@users.noreply.github.com>
2021-01-22 20:06:04 +0000

    commands

--- base/initialise.py
+++ base/initialise.py
@@ -1,31 +1,31 @@
+from typing import Iterable, List
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
+from math import log
+import maths_command
 
-client = discord.Client()
-
-#* response once logged on
-@client.event
-async def on_ready():
-    print(f'Connected to Discord as {client.user}')
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    print(f'Connected to Guild: {guild.name} with ID {guild.id}')
+bot = commands.Bot(command_prefix='.')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('.hello'):
-        await message.channel.send('Ready!')
+@bot.event
+async def on_ready(): #* response once logged on
+    print(f'Connected to Discord as {bot.user}')
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(f'Connected to Guild: {guild.name} [ID: {guild.id}]')
 
 
 
+
+@bot.command(name='ready', help="Gets a response if the bot's working")
+async def on_message(context):  #* ready message
+    await context.send('Ready!')
+
+#* startup
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 GUILD = os.getenv('GUILD')
 
 print('...',end='\r',flush=True)
-client.run(TOKEN)
+bot.run(TOKEN)
