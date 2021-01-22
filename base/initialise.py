f7faaf874692195636e963b21a220ded93cc479e
StreakSharn <74095821+StreakSharn@users.noreply.github.com>
2021-01-22 12:36:43 +0000

    guilds

--- base/initialise.py
+++ base/initialise.py
@@ -1,12 +1,18 @@
 import discord
 import os
+from dotenv import load_dotenv
 
 client = discord.Client()
 
 #* response once logged on
 @client.event
 async def on_ready():
-    print('Logged in!')
+    print(f'Connected to Discord as {client.user}')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'Connected to Guild: {guild.name} with ID {guild.id}')
+
 
 @client.event
 async def on_message(message):
@@ -15,5 +21,11 @@ async def on_message(message):
     if message.content.startswith('.hello'):
         await message.channel.send('Ready!')
 
-token = os.getenv('Discord_Token')
-client.run(token)
+
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('GUILD')
+
+print('...',end='\r',flush=True)
+client.run(TOKEN)
